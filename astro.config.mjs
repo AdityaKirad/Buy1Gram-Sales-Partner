d8e27fcaba1151aecb911af4068ebc8ce3857b77
AdityaKirad <kiradaditya2001@gmail.com>
2023-08-20 18:27:32 +0530

    changed sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://buy1gram-suppliers.pages.dev/",
+  site: "https://buy1gram-sales-partner.pages.dev/",
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
   experimental: {
     assets: true,
